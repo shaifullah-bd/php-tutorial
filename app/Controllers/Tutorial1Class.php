37029a81d96b2682c9285338aa587d54a4d88607
bdpr17 <bdtask.pr17@gmail.com>
2023-07-13 19:23:59 +0600

    Enum learning

--- app/Controllers/Tutorial1Class.php
+++ app/Controllers/Tutorial1Class.php
@@ -11,11 +11,59 @@ namespace App\Controllers;
 /**
  * We are learn about comments, variable, echo, print, data types, string, numbers, math, constants, operators, if-else, switch, loops, function, arrays, superglobals
  */
-class Tutorial1Class {
-    
-  public function printMsg(string $msgData = null):string
-  {
-    return $msgData;
-  }
-  
+class Tutorial1Class 
+{
+    public function echoPrint(): void
+    {
+        echo "Hello World";
+        print("<br> Hello World");
+
+        $result = print "Hello World";
+        echo '<br>'.$result;
+    }
+
+    /**
+     * @return string
+     */
+    public function stringDataType(): string
+    {
+        return "Hello World From String Double Quote".' Hello World From String Single Quote';
+    }
+
+    /**
+     * @param int $number
+     * @return Integer
+     */
+    public function integerDataType(int $number): int
+    {
+        $number += 1;
+        return $number;
+    }
+
+    /**
+     * @param float $number1
+     * @return float
+     */
+    public function floatDataType(float $number1): float
+    {
+        $number1 += 1.2;
+        return $number1;
+    }
+
+    /**
+     * @param bool $status
+     * @return bool
+     */
+    public function boolDataType(bool $status, string $msgData): bool
+    {
+        if ($status === true) {
+            echo $msgData;
+        } elseif ($status === false) {
+            echo "Status is False";
+        } else {
+
+        }
+
+        return $status;
+    }
 }
---
+++ app/Controllers/Tutorial2Class.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * We are learn php basic syntax chapter 2
+ */
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+/**
+ * We are learn PHP FORM by this class
+ */
+class Tutorial2Class
+{
+    public function getMessage(): void
+    {
+        echo "Tutorial2Class Printed Successfully";
+    }
+}
